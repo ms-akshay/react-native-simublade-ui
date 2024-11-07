38edd0b008b7280609e5568a987b1e654ed75302
Akshay Maheshwari <akshay26072000@gmail.com>
2024-11-07 15:32:55 +0530

    fixed import issue

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,7 @@ export { default as Button } from "./Button/Button";
 export { default as IconButton } from "./IconButton/IconButton";
 export { default as SearchBar } from "./SearchBar/SearchBar";
 export { default as Typography } from "./Typography/Typography";
-export { default as SnackBar } from "./Snackbar/SnackBar";
+export { default as SnackBar } from "./SnackBar/SnackBar";
 export { default as AppIntroSlider } from "./AppIntroSlider/AppIntroSlider";
 export { default as Badge } from "./Badge/Badge";
 export { default as MultiSelectDropdown } from "./MultiSelectDropdown/MultiSelectDropdown";
